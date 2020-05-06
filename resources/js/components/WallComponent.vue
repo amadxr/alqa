5778f35c25ad0690be078846565560491b6c8440
amador <joamadorrr@gmail.com>
2020-05-05 21:58:50 -0500

    Project setup and detect cursor position

---
+++ resources/js/components/WallComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-on:mousemove="getMousePosition" class="vh-100 d-flex justify-content-center align-items-center">
+        <h1>ALQA</h1>                          
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {   
+                mousePosition: {
+                    x: 0,
+                    y: 0,
+                }             
+            };
+        },
+        methods: {
+            getMousePosition: function (event){
+                console.log(event.clientX, event.clientY);
+                this.mousePosition.x = event.clientX;
+                this.mousePosition.y = event.clientY;
+            }
+        }
+    }
+</script>
