59dfbd7424db5b1aafa6c1270af34e63992b340a
AMADOR <43796575+amadxr@users.noreply.github.com>
2020-09-07 19:04:10 -0500

    Refactor styles and change card size

--- resources/js/components/ModalComponent.vue
+++ resources/js/components/ModalComponent.vue
@@ -1,14 +1,14 @@
 <template>
     <transition name="modal">
-        <div class="fixed top-0 left-0 bg-white bg-opacity-25 z-10 h-screen w-screen flex items-center justify-center"
+        <div class="fixed top-0 left-0 z-10 flex items-center justify-center w-screen h-screen bg-white bg-opacity-25"
             @click="closeModal">
-            <div class="w-25 px-10 py-20 bg-wall border-solid border border-wall-alt">
-                <div class="pb-32 first-p-color text-center font-alqa-special text-first-p-size leading-13">
+            <div class="px-10 py-20 border border-solid h-card w-card bg-wall border-wall-alt">
+                <div class="pb-20 text-center first-p-color font-alqa-special text-first-p-size leading-11">
                     <slot name="first">
                         default first paragraph
                     </slot>
                 </div>
-                <div class="px-12 second-p-color text-center text-second-p-size font-alqa-special italic leading-8">
+                <div class="px-12 italic text-center second-p-color text-second-p-size font-alqa-special leading-8">
                     <slot name="second">
                         default second paragraph
                     </slot>
